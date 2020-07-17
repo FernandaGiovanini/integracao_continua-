9a4a873f3df31995b79d1029f4451a61e02dc8e4
FernandaGiovanini <fer_giovanini@hotmail.com>
2020-07-17 11:08:11 -0300

    integracao

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+
+var Cliente = require('../models/cliente');
+
 var router = express.Router();
 
+
+
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fernanda Giovanini' });
+
+  let cpf = req.query.cpf
+
+  var cliente = new Cliente()
+
+  cliente.cpf = cpf
+
+  let cpfValidado = cliente.validarCPF()
+
+  res.render('index', { title: 'Suppi Store', cpfValidado:cpfValidado, cpf:cpf });
+
 });
+
+
 
 module.exports = router;
